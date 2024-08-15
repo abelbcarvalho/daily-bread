2e339432d39b8fb2bf1dd50c41654ee0fec12de5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-15 14:45:39 -0300

    feat: checker digits calculation for cnpj

---
+++ src/utilities/checkers/CnpjChecker.ts
@@ -0,0 +1,49 @@
+export const checkCnpj = (cnpj: string): void => {
+    const regex = "^[0-9]{14}$";
+
+    const tester = new RegExp(regex);
+
+    if (!tester.test(cnpj)) {
+        throw new Error("cnpj digits are invalid");
+    }
+};
+
+export const checkCnpjDigits = (cnpj: string): void => {
+    const checkFirst = cnpj.substring(0, 12).split("");
+    const checkSecond = cnpj.substring(0, 13).split("");
+
+    const digitsToCheck = cnpj.substring(12, 14);
+
+    let mult = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+    let frist = 0, second = 0;
+
+    let id = 0;
+
+    checkFirst.forEach((x) => {
+        let numItem = mult[id];
+        frist += parseInt(x) * numItem;
+        id += 1;
+    });
+
+    mult = (6 + mult.join("")).split("").map((x) => parseInt(x));
+    id = 0;
+
+    checkSecond.forEach((x) => {
+        let numItem = mult[id];
+        second += parseInt(x) * numItem;
+        id += 1;
+    });
+
+    const restFirst = 11 - frist % 11;
+    const restSecond = 11 - second % 11;
+
+    let digits = "";
+
+    digits += restFirst >= 10 ? 0 : restFirst;
+    digits += restSecond >= 10 ? 0 : restSecond;
+
+    if (!(digitsToCheck === digits)) {
+        throw new Error("cnpj checker digits are invalid");
+    }
+};
