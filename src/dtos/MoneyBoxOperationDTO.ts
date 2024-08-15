2f54040a7491977c8d8e1332ec40d8ad69b47169
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-15 18:42:12 -0300

    feat: money box operation dto created

---
+++ src/dtos/MoneyBoxOperationDTO.ts
@@ -0,0 +1,8 @@
+import { EnumMoney } from "../enumerates/EnumMoney";
+
+export interface MoneyBoxOperationDTO {
+    descript: string;
+    typeOpr: EnumMoney;
+    balance: number;
+    moneyBoxId: number;
+}
