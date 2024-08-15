84ef04300ed620892ab1bcb8df2a32f47a105364
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-15 18:33:36 -0300

    feat: money operation dto created

---
+++ src/dtos/MoneyOperationDTO.ts
@@ -0,0 +1,8 @@
+import { EnumMoney } from "../enumerates/EnumMoney";
+
+export interface MoneyOperationDTO {
+    descript: string;
+    typeOpr: EnumMoney;
+    balance: number;
+    moneyId: number;
+}
