a809d5a1f4e9e5d530555ce528c97f3e008edf86
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-15 18:17:13 -0300

    feat: account dto created

---
+++ src/dtos/AccountDTO.ts
@@ -0,0 +1,15 @@
+import { EnumGender } from "../enumerates/EnumGender";
+import { EnumLegalPerson } from "../enumerates/EnumLegalPerson";
+
+export interface AccountDTO {
+    name: string;
+    gender: EnumGender;
+    socialName?: string;
+    person: EnumLegalPerson;
+    cpf?: string;
+    cnpj?: string;
+    email: string;
+    username: string;
+    password: string;
+    mobile: string;
+}
