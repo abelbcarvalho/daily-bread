749b89b33707e1dc09a506f7a03037e71e632668
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 18:10:27 -0300

    feat: pix random key exception checkers

--- src/utilities/checkers/RandomPixKeyChecker.ts
+++ src/utilities/checkers/RandomPixKeyChecker.ts
@@ -1,9 +1,11 @@
+import { PixException } from "../../exceptions/PixException";
+
 export const checkRandomPixKey = async (pixKey: string): Promise<void> => {
     const regex = "^[A-Z0-9]{8,32}$";
 
     const tester = RegExp(regex);
 
     if (!tester.test(pixKey)) {
-        throw new Error("random pix key invalid");
+        throw new PixException("random pix key invalid");
     }
 };
