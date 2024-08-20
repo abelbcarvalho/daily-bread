cdaed78ec8151a3cbefdf6e8125ee34bf342f22a
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 19:01:18 -0300

    feat: model money operation box created

---
+++ src/models/MoneyOprBox.ts
@@ -0,0 +1,9 @@
+export class MoneyOprBox {
+    public id: number = 0;
+
+    public moneyOprId: number = 0;
+    public moneyBoxOprId: number = 0;
+
+    public createdAt: Date = new Date(2000, 1, 1);
+    public updatedAt: Date = new Date(2000, 1, 1);
+}
