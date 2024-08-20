245051a693f691ad009e4c45d3335289aa0692d8
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 18:45:33 -0300

    feat: model bank operation payment created

---
+++ src/models/BankOprPayment.ts
@@ -0,0 +1,9 @@
+export class BankOprPayment {
+    public id: number = 0;
+
+    public bankOprId: number = 0;
+    public paymentId: number = 0;
+
+    public createdAt: Date = new Date(2000, 1, 1);
+    public updatedAt: Date = new Date(2000, 1, 1);
+}
