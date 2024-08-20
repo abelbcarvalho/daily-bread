1317061a5aadde60b4f51bab34bf553f4ccd4229
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 18:09:58 -0300

    feat: mobile exception checkers

--- src/utilities/checkers/MobileChecker.ts
+++ src/utilities/checkers/MobileChecker.ts
@@ -1,9 +1,11 @@
+import { MobileException } from "../../exceptions/MobileException";
+
 export const checkMobileNumber = async (mobile: string): Promise<void> => {
     const regex = "^\\+[0-9]{13}$";
 
     const tester = new RegExp(regex);
 
     if (!tester.test(mobile)) {
-        throw new Error("mobile number digits invalid");
+        throw new MobileException("mobile number digits invalid");
     }
 };
