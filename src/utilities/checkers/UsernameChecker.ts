4617aaaf860cf0638ac4a27dff235a635b96743f
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 18:10:56 -0300

    feat: username exception checkers

--- src/utilities/checkers/UsernameChecker.ts
+++ src/utilities/checkers/UsernameChecker.ts
@@ -1,9 +1,11 @@
+import { UsernameException } from "../../exceptions/UsernameException";
+
 export const checkUsername = async (username: string): Promise<void> => {
     const regex = "^[a-z\._]{1}[a-z\._0-9]{3,32}$";
 
     const tester = new RegExp(regex);
 
     if (!tester.test(username)) {
-        throw new Error("username characters are invalid")
+        throw new UsernameException("username characters are invalid")
     }
 };
