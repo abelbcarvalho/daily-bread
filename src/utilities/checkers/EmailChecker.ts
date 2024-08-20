df0656e0840f12ccdca7cd12a2b32db8a3c86b2b
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 18:09:27 -0300

    feat: email exception checkers

--- src/utilities/checkers/EmailChecker.ts
+++ src/utilities/checkers/EmailChecker.ts
@@ -1,9 +1,11 @@
+import { EmailException } from "../../exceptions/EmailException";
+
 export const checkEmail = async (email: string): Promise<void> => {
     const regex = "^[a-z\-_0-9]{1}[a-z\-_0-9.\+]{0,62}[a-z\-_0-9]{1}\@[a-z\-_0-9]{1}[a-z\-_0-9.\+]{0,177}\.[a-z]{2,10}$";
 
     const tester = new RegExp(regex);
 
     if (!tester.test(email)) {
-        throw new Error("email characters are invalid");
+        throw new EmailException("email characters are invalid");
     }
 };
