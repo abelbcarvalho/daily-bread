5845944d40ff83cfb2821601f958adfdd225d8c1
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-20 11:45:43 -0300

    feat: interest calculation created

--- src/core/calculators/InterestCalculator.ts
+++ src/core/calculators/InterestCalculator.ts
@@ -3,6 +3,12 @@ export class InterestCalculator {
     private capitalMoney: number;
     private timeInvested: number;
 
+    constructor() {
+        this.interestRate = 0;
+        this.capitalMoney = 0;
+        this.timeInvested = 0;
+    }
+
     get interest(): number {
         return this.interestRate * 100;
     }
@@ -31,9 +37,39 @@ export class InterestCalculator {
         this.interest = 100 + this.interest;
     }
 
-    constructor() {
-        this.interestRate = 0;
-        this.capitalMoney = 0;
-        this.timeInvested = 0;
+    async commonInterestCalc(capital: number, interest: number, time: number): Promise<number> {
+        this.capital = capital;
+        this.interestRate = interest;
+        this.time = time;
+    
+        await this.toCompoundRate();
+
+        return (
+            this.capital * this.interest * this.time
+        ) / 100;
+    }
+
+    async commonInterestAmount(capital: number, interest: number, time: number): Promise<number> {
+        const totalInterest = await this.commonInterestCalc(capital, interest, time);
+
+        return this.capital + totalInterest;
+    }
+
+    async compoundInterestCalc(capital: number, interest: number, time: number): Promise<number> {
+        this.capital = capital;
+        this.interestRate = interest;
+        this.time = time;
+
+        await this.toCompoundRate();
+
+        return (
+            this.capital * (100 + this.interest) ** this.time
+        ) / 100 ** this.time;
+    }
+
+    async compoundInterestAmount(capital: number, interest: number, time: number): Promise<number> {
+        const totalInterest = await this.compoundInterestCalc(capital, interest, time);
+
+        return this.capital + totalInterest;
     }
 }
