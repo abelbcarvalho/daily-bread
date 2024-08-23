642b6acad20867020d5e9eb83a6c153fb3fc6ae4
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-23 09:14:41 -0300

    feat: constoller account skeleton

---
+++ src/controllers/ControllerAccount.ts
@@ -0,0 +1,12 @@
+export class ControllerAccount {
+    constructor() { }
+
+    async createNewAccount(response: Response, request: Request): Promise<any> {
+    }
+
+    async makeLoginExistingAccount(response: Response, request: Request): Promise<any> {
+    }
+
+    async deactiveExistingAccount(response: Response, request: Request): Promise<any> {
+    }
+}
