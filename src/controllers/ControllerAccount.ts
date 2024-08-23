369c240b0935ac816eeb063c69db9fe2834f1161
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-23 10:58:07 -0300

    feat: controller account linked to their service

--- src/controllers/ControllerAccount.ts
+++ src/controllers/ControllerAccount.ts
@@ -1,25 +1,36 @@
 import { AddapterAccountDTO } from "@utilities/dto-adapters/AdapterAccountDTO";
 import { AddapterAccountLoginDTO } from "@utilities/dto-adapters/AdapterAccountLoginDTO";
 import { AdapterRequestParam } from '../utilities/dto-adapters/AdapterRequestParam';
+import { ServiceAccount } from "@services/ServiceAccount";
 
 export class ControllerAccount {
-    constructor() { }
+    private service: ServiceAccount;
+
+    constructor() {
+        this.service = new ServiceAccount();
+    }
 
     async createNewAccount(response: Response, request: Request): Promise<any> {
         const adapter = new AddapterAccountDTO(request);
 
         const account = await adapter.adapterAccountDTO();
+
+        return await this.service.createNewAccount(account);
     }
 
     async makeLoginExistingAccount(response: Response, request: Request): Promise<any> {
         const adapter = new AddapterAccountLoginDTO(request);
 
         const loginAccount = await adapter.adapterAccountLoginDTO();
+
+        return await this.service.makeLoginExistingAccount(loginAccount);
     }
 
     async deactiveExistingAccount(response: Response, request: Request): Promise<any> {
         const adapter = new AdapterRequestParam(request);
 
         const accountId = await adapter.getParamID();
+
+        return await this.service.deactiveExistingAccount(accountId);
     }
 }
