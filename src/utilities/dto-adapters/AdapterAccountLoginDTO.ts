46a87c20614faacf07c56079f275576b14c7418b
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-23 09:31:58 -0300

    feat: account login dto created * dto; * exception; * adapter

---
+++ src/utilities/dto-adapters/AdapterAccountLoginDTO.ts
@@ -0,0 +1,39 @@
+import { AccountLoginDTO } from "@dtos/AccountLoginDTO";
+import { BodyException } from "@exceptions/BodyException";
+
+export class AddapterAccountLoginDTO {
+    private body: any;
+
+    constructor(request: Request) {
+        this.body = request.body === null ? null : request.body;
+    }
+
+    private async isBodyNull(): Promise<void> {
+        if (this.body === null) {
+            throw new BodyException("request body can't be null");
+        }
+
+        this.body = this.body as AccountLoginDTO;
+    }
+
+    private async isAccountLoginDTORequired(): Promise<boolean> {
+        const requiredValidators = new Map<string, (value: any) => boolean>([
+            ['username', value => typeof value === 'string'],
+            ['password', value => typeof value === 'string'],
+        ]);
+
+        return Array.from(requiredValidators.entries()).every(([key, validate]) => validate(this.body[key]));
+    }
+
+    async adapterAccountLoginDTO(): Promise<AccountLoginDTO> {
+        await this.isBodyNull();
+
+        const requires = await this.isAccountLoginDTORequired();
+
+        if (requires === false) {
+            throw new BodyException("body is invalid for account login");
+        }
+
+        return this.body;
+    }
+}
