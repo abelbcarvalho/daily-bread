44bb5adbc8359dacd3f26e81ced3aa2d96356791
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-23 09:15:27 -0300

    feat: adapter request body to dto on controller

--- src/controllers/ControllerAccount.ts
+++ src/controllers/ControllerAccount.ts
@@ -1,7 +1,12 @@
+import { AddapterAccountDTO } from "@utilities/dto-adapters/AdapterAccountDTO";
+
 export class ControllerAccount {
     constructor() { }
 
     async createNewAccount(response: Response, request: Request): Promise<any> {
+        const adapter = new AddapterAccountDTO(request);
+
+        const account = await adapter.adapterAccountDTO();
     }
 
     async makeLoginExistingAccount(response: Response, request: Request): Promise<any> {
