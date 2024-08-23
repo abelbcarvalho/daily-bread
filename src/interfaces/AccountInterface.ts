57c78307ebe4bd989e89302d3cf6b21528f8473c
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-23 11:10:28 -0300

    feat: interface account with required methods

---
+++ src/interfaces/AccountInterface.ts
@@ -0,0 +1,8 @@
+import { AccountDTO } from "@dtos/AccountDTO";
+import { AccountLoginDTO } from "@dtos/AccountLoginDTO";
+
+export interface AccountInterface {
+    createNewAccount(account: AccountDTO): Promise<any>;
+    makeLoginExistingAccount(accountLogin: AccountLoginDTO): Promise<any>
+    deactiveExistingAccount(accountId: number): Promise<any>
+}
