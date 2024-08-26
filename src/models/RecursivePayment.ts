986eba7b9dcc61f70f722606cad77392f6e1573c
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-26 12:36:48 -0300

    feat: recursive payment model created

---
+++ src/models/RecursivePayment.ts
@@ -0,0 +1,17 @@
+import { EnumStageRepeat } from "@enumerates/EnumStageRepeat";
+
+export class RecuersivePayment {
+    public id: number = 0;
+
+    public descript: string = "";
+    public balance: number = 0;
+    public day: number = 0;
+    public month: number = 0;
+    public stage: EnumStageRepeat = EnumStageRepeat.NO_REPEAT;
+    public finish: boolean = false;
+
+    public accountID: number = 0;
+
+    public createdAt: Date = new Date(2000, 1, 1);
+    public updatedAt: Date = new Date(2000, 1, 1);
+}
