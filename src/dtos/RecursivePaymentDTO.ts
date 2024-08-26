9c4373c91b0b0e3612cb39c443d6da9e686e40fa
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-26 12:31:22 -0300

    feat: recursive payment dto created

---
+++ src/dtos/RecursivePaymentDTO.ts
@@ -0,0 +1,12 @@
+import { EnumStageRepeat } from "@enumerates/EnumStageRepeat";
+
+export interface RecursivePaymentDTO {
+    descript: string;
+    balance: number;
+    day: number;
+    month: number;
+    stage: EnumStageRepeat;
+    finish: boolean;
+
+    accountID: number;
+}
