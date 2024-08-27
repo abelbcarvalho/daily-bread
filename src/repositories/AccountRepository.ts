d9220557f1060a0c2183546c2e23549a6367f02a
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-27 18:07:18 -0300

    feat: repository login part created

--- src/repositories/AccountRepository.ts
+++ src/repositories/AccountRepository.ts
@@ -1,5 +1,5 @@
 import { AccountLoginDTO } from "@dtos/AccountLoginDTO";
-import { EnumGender, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { DatabaseException } from "@exceptions/DatabaseException";
 import { AccountInterfaceRepository } from "@interfaces/AccountInterfaceRepository";
 import { Account } from "@domain/Account";
@@ -27,11 +27,40 @@ export class AccountRepository implements AccountInterfaceRepository {
             throw new DatabaseException((error as Error).message, 422);
         }
         finally {
-            this.prisma.$disconnect;
+            this.prisma.$disconnect();
         }
     }
 
     async makeLoginExistingAccount(accountLogin: AccountLoginDTO): Promise<any> {
+        try {
+            await this.prisma.$connect();
+
+            let existingAccount: any;
+
+            await this.prisma.$transaction(async (db) => {
+                existingAccount = await db.account.findFirst({
+                    where: {
+                        AND: [
+                            {
+                                OR: [
+                                    { username: accountLogin.username },
+                                    { email: accountLogin.username }
+                                ]
+                            },
+                            { password: accountLogin.password }
+                        ]
+                    }
+                });
+            });
+
+            return existingAccount;
+        }
+        catch (error) {
+            throw new DatabaseException((error as Error).message, 422);
+        }
+        finally {
+            this.prisma.$disconnect();
+        }
     }
 
     async deactiveExistingAccount(accountId: number): Promise<any> {
