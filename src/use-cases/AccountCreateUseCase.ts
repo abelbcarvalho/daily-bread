bd110da6e6a962a962ad01c45530b85bda0dae06
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-27 11:18:49 -0300

    feat: create account use case

---
+++ src/use-cases/AccountCreateUseCase.ts
@@ -0,0 +1,18 @@
+import { AccountDTO } from "@dtos/AccountDTO";
+import { AddapterAccountDTO } from '../utilities/dto-adapters/AdapterAccountDTO';
+import { AccountInterfaceRepository } from "@interfaces/AccountInterfaceRepository";
+import { AccountRepository } from "src/repositories/AccountRepository";
+
+export class AccountCreateUseCase {
+    private repository: AccountInterfaceRepository;
+
+    constructor() {
+        this.repository = new AccountRepository();
+    }
+
+    async execute(account: AccountDTO): Promise<any> {
+        const accountCreate = await AddapterAccountDTO.adaptAccountDTOToDomain(account);
+
+        return await this.repository.createNewAccount(accountCreate);
+    }
+}
