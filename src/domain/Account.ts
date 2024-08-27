731ac5c61b5c7131d3610829d6486c2e5d08d307
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-08-27 10:46:40 -0300

    feat: created domain account and structure

---
+++ src/domain/Account.ts
@@ -0,0 +1,18 @@
+import {
+    EnumGender,
+    EnumLegalPerson,
+} from "@prisma/client";
+
+export type Account {
+    name: string;
+    gender: EnumGender;
+    socialName?: string | undefined;
+    person: EnumLegalPerson;
+    cpf?: string | undefined;
+    cnpj?: string | undefined;
+    email: string;
+    username: string;
+    password: string;
+    mobile: string;
+    active?: boolean | undefined;   
+}
