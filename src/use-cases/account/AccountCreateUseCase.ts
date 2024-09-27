68858672a914d8584b5640ab5798e24c9e71458d
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-09-27 09:30:35 -0300

    feat: resource for hash password working well

--- src/use-cases/account/AccountCreateUseCase.ts
+++ src/use-cases/account/AccountCreateUseCase.ts
@@ -2,6 +2,7 @@ import { AccountDTO } from "@dtos/AccountDTO";
 import { AddapterAccountDTO } from "@utilities/dto-adapters/AdapterAccountDTO";
 import { AccountInterfaceRepository } from "@interfaces/AccountInterfaceRepository";
 import { AccountRepository } from "@repositories/AccountRepository";
+import { HashPassword } from "@utilities/security/HashPassword";
 
 export class AccountCreateUseCase {
     private repository: AccountInterfaceRepository;
@@ -12,6 +13,8 @@ export class AccountCreateUseCase {
 
     async execute(account: AccountDTO): Promise<any> {
         const accountCreate = await AddapterAccountDTO.adaptAccountDTOToDomain(account);
+
+        accountCreate.password = await HashPassword.hashPassword(accountCreate.password);
 
         const newAccount = await this.repository.createNewAccount(accountCreate);
 
