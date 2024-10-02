82c6b3056568017075bfbbb45f8a789baf4bd0c5
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-02 11:26:23 -0300

    feat: controller and auth controller

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -3,7 +3,7 @@ import {
     Response,
     NextFunction
 } from "express";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key";
 
@@ -15,11 +15,12 @@ export const tokenAuthentication = async (request: Request, response: Response, 
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
         if (err) return response.sendStatus(403);
-        request.body = user;
+
+        request.params["id"] = (user as JwtPayload).id;
         next();
     });
 };
 
-export const generateToken = async (payload: any, expiresIn: string | number = "1h"): Promise<string> => {
+export const generateToken = async (payload: any, expiresIn: string | number = "1m"): Promise<string> => {
     return jwt.sign(payload, SECRET_KEY, { expiresIn });
 };
