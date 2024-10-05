8ff3852782f1851fe3ed1301536b65e7cd612a0c
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-05 13:54:04 -0300

    feat: controller model validate to recursive payment context ready

--- src/controllers/ControllerRecursivePay.ts
+++ src/controllers/ControllerRecursivePay.ts
@@ -1,5 +1,9 @@
+import { BaseException } from "@exceptions/BaseException";
 import { RecursivePayInteface } from "@interfaces/RecursivePayInterface";
 import { ServiceRecursivePay } from "@services/ServiceRecursivePay";
+import { getAccountIdFromRequestBody } from "@utilities/data-getter/DataGetter";
+import { AdapterRecursivePaymentDTO } from "@utilities/dto-adapters/AdapterRecursivePaymentDTO";
+import { AdapterRequestParam } from "@utilities/dto-adapters/AdapterRequestParam";
 import { Request, Response } from "express";
 
 export class ControllerRecursivePay {
@@ -9,9 +13,64 @@ export class ControllerRecursivePay {
         this.service = new ServiceRecursivePay();
     }
 
-    async createNewRecursivePayment(response: Response, request: Request): Promise<any> { }
+    async createNewRecursivePayment(response: Response, request: Request): Promise<any> {
+        try {
+            const accountId = await getAccountIdFromRequestBody(request);
 
-    async updateAnExistingRecursivePayment(response: Response, request: Request): Promise<any> { }
+            const adapter = new AdapterRecursivePaymentDTO(request);
+            const recursivePay = await adapter.adapterRecursivePayDTO();
 
-    async getAllRecursivePaymentsByAccountId(response: Response, request: Request): Promise<any> { }
+            recursivePay.accountId = accountId;
+            request.body = {};
+
+            const newRecursivePay = await this.service.createNewRecursivePayment(recursivePay);
+
+            return response.status(200).send(newRecursivePay);
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
+    }
+
+    async updateAnExistingRecursivePayment(response: Response, request: Request): Promise<any> {
+        try {
+            const accountId = await getAccountIdFromRequestBody(request);
+
+            const adapter = new AdapterRequestParam(request);
+
+            const recurPayId = await adapter.getParamID();
+
+            const adaptMoney = new AdapterRecursivePaymentDTO(request);
+            const recursivePay = await adaptMoney.adapterRecursivePayDTO();
+            recursivePay.accountId = accountId;
+
+            request.body = {};
+
+            const moneyCash = await this.service.updateAnExistingRecursivePayment(recursivePay, recurPayId);
+
+            return response.status(200).send(moneyCash);
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
+    }
+
+    async getAllRecursivePaymentsByAccountId(response: Response, request: Request): Promise<any> {
+        try {
+            const accountId = await getAccountIdFromRequestBody(request);
+            request.body = {}
+
+            const allRecursivePays = await this.service.getAllRecursivePaymentsByAccountId(accountId);
+
+            const status = allRecursivePays.length > 0 ? 200 : 404;
+
+            return response.status(status).send({ moneyCash: allRecursivePays });
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
+    }
 }
