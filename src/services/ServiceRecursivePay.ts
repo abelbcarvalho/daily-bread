09e558f7f0336ab6af0db870e8e24e3e85bcb786
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-05 15:53:37 -0300

    feat: recursive payment checker

--- src/services/ServiceRecursivePay.ts
+++ src/services/ServiceRecursivePay.ts
@@ -3,6 +3,7 @@ import { RecursivePayInteface } from "@interfaces/RecursivePayInterface";
 import { RecursivePayCreateUseCase } from "@use-cases/recursive-payment/RecursivePayCreateUseCase";
 import { RecursivePayGetAllUseCase } from "@use-cases/recursive-payment/RecursivePayGetAllUseCase";
 import { RecursivePayUpdateUseCase } from "@use-cases/recursive-payment/RecursivePayUpdateUseCase";
+import { recursivePayChecker, recursivePayCheckerUpdate } from "@utilities/checkers/RecursivePayChecker";
 
 export class ServiceRecursivePay implements RecursivePayInteface {
     private create: RecursivePayCreateUseCase;
@@ -16,10 +17,14 @@ export class ServiceRecursivePay implements RecursivePayInteface {
     }
 
     async createNewRecursivePayment(recursivePay: RecursivePaymentDTO): Promise<any> {
+        await recursivePayChecker(recursivePay);
+
         return await this.create.execute(recursivePay);
     }
 
     async updateAnExistingRecursivePayment(recursivePay: RecursivePaymentDTO, recPayId: number): Promise<any> {
+        await recursivePayCheckerUpdate(recursivePay);
+
         return await this.update.execute(recursivePay, recPayId);
     }
 
