3eda97e6f71d34c0c191ccea388f8324daccd4e7
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-07 15:50:29 -0300

    feat: basic controller bank box structure

--- src/controllers/ControllerBankBox.ts
+++ src/controllers/ControllerBankBox.ts
@@ -1,3 +1,4 @@
+import { BaseException } from "@exceptions/BaseException";
 import { BankBoxInterface } from "@interfaces/BankBoxInterface";
 import { ServiceBankBox } from "@services/ServiceBankBox";
 import { Request, Response } from "express";
@@ -10,14 +11,47 @@ export class ControllerBankBox {
     }
 
     async createNewBankBox(response: Response, request: Request): Promise<any> {
-        throw new Error("Method not implemented.");
+        try {
+            const bankBox = request.body;
+
+            const result = await this.service.createNewBankBox(bankBox);
+
+            return response.status(200).send(result);
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
     }
 
     async updateAnExistingBankBox(response: Response, request: Request): Promise<any> {
-        throw new Error("Method not implemented.");
+        try {
+            const bankBox = request.body;
+            const bankBoxId = parseInt(request.params["id"]);
+
+            const result = await this.service.updateAnExistingBankBox(bankBox, bankBoxId);
+
+            return response.status(200).send(result);
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
     }
 
     async getAllBankBoxByBankId(response: Response, request: Request): Promise<any> {
-        throw new Error("Method not implemented.");
+        try {
+            const bankId = parseInt(request.params["bankId"]);
+
+            const result = await this.service.getAllBankBoxByBankId(bankId);
+
+            const status = result.length > 0 ? 200 : 404;
+
+            return response.status(status).send(result);
+        }
+        catch (error) {
+            const err = error as BaseException;
+            return response.status(err.code).send({ error: err.message });
+        }
     }
 }
