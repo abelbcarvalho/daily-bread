991563e1293d350940a28efb712264507ea531f3
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-10-07 16:07:21 -0300

    feat: linked it use case to the service of bank box

--- src/services/ServiceBankBox.ts
+++ src/services/ServiceBankBox.ts
@@ -1,18 +1,29 @@
 import { BankBoxDTO } from "@dtos/BankBoxDTO";
 import { BankBoxInterface } from "@interfaces/BankBoxInterface";
+import { BankBoxCreateUseCase } from "@use-cases/bank-box/BankBoxCreateUseCase";
+import { BankBoxGetAllUseCase } from "@use-cases/bank-box/BankBoxGetAllUseCase";
+import { BankBoxUpdateUseCase } from "@use-cases/bank-box/BankBoxUpdateUseCase";
 
 export class ServiceBankBox implements BankBoxInterface {
-    constructor() { }
+    private create: BankBoxCreateUseCase;
+    private update: BankBoxUpdateUseCase;
+    private getAll: BankBoxGetAllUseCase;
+
+    constructor() {
+        this.create = new BankBoxCreateUseCase();
+        this.update = new BankBoxUpdateUseCase();
+        this.getAll = new BankBoxGetAllUseCase();
+    }
 
     async createNewBankBox(bankBox: BankBoxDTO): Promise<any> {
-        throw new Error("Method not implemented.");
+        return await this.create.execute(bankBox);
     }
 
     async updateAnExistingBankBox(bankBox: BankBoxDTO, bankBoxId: number): Promise<any> {
-        throw new Error("Method not implemented.");
+        return await this.update.execute(bankBox, bankBoxId);
     }
 
     async getAllBankBoxByBankId(bankId: number): Promise<any> {
-        throw new Error("Method not implemented.");
+        return await this.getAll.execute(bankId);
     }
 }
